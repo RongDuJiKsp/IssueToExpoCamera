ea2c456265c91af39f2c59e63785c7528b407685
周钰琪 <rdjksp@qq.com>
2023-11-10 09:25:38 +0800

    This is the min project

---
+++ app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Stack } from 'expo-router/stack';
+
+export default function Layout() {
+    return <Stack screenOptions={{
+        headerShown:false
+    }} />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import {Button, View} from "react-native";
+import {Camera} from "expo-camera";
+import {router} from "expo-router";
+import {useState} from "react";
+
+export default function App() {
+    const [camera, setCamera] = useState<Camera|null>(null);
+    return <View>
+        <Button title={"GETPERMMISION"} onPress={() => {
+            Camera.requestCameraPermissionsAsync().then();
+        }}/>
+        <Button title={"GETPERMMISION"} onPress={() => {
+            Camera.requestCameraPermissionsAsync().then();
+
+        }}/>
+        <Button title={"TO PAGE"} onPress={() => {
+            router.push("/other");
+        }}/>
+        <Camera style={{height: "100%"}} ref={(e) => setCamera(e)}/>
+    </View>
+}
