654a01bed61a39ff8ca0ed99b8646eeb53b7242a
溶度积Ksp <rdjksp@gmail.com>
2024-01-28 12:56:04 +0800

    USE API2

--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,27 @@ import {Button, View} from "react-native";
 import {Camera} from "expo-camera";
 import {router} from "expo-router";
 import {useState} from "react";
+import {CameraView} from "expo-camera/next";
 
 export default function App() {
-    const [camera, setCamera] = useState<Camera|null>(null);
+    const [isShowing,setShowing]=useState<boolean>(false);
     return <View>
         <Button title={"GETPERMMISION"} onPress={() => {
             Camera.requestCameraPermissionsAsync().then();
         }}/>
         <Button title={"GETPERMMISION"} onPress={() => {
             Camera.requestCameraPermissionsAsync().then();
-
+        }}/>
+        <Button title={"close"} onPress={() => {
+            setShowing(false);
+        }}/>
+        <Button title={"open"} onPress={() => {
+           setShowing(true);
         }}/>
         <Button title={"TO PAGE"} onPress={() => {
             router.push("/other");
         }}/>
-        <Camera useCamera2Api={true} style={{height: "100%"}} ref={(e) => setCamera(e)}/>
+
+        {isShowing&&  <CameraView style={{height: "100%"}}/>}
     </View>
 }
